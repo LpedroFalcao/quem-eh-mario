f71fa7d7e58880b77bc1238677bb9cfd92d48595
Pedro Falcão <plfalcaolima@gmail.com>
2023-07-30 23:13:25 -0300

    gameplay complete

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-const mario = document.querySelector('.Qmario')
+const mario = document.querySelector('.Qmario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
     mario.classList.add('jump')
 
     setTimeout(() => {mario.classList.remove('jump')},500)
 }
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const QmarioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    if(pipePosition <= 120 && pipePosition > 0 && QmarioPosition < 80){
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`
 
+        mario.style.animation = 'none';
+        mario.style.left = `${QmarioPosition}px`;
+
+        mario.src ='./img/game-over.png'
+        mario.style.width='75px';
+        mario.style.marginLeft='50px'
+
+        clearInterval(loop);
+    }
+},10);
 document.addEventListener('keydown',jump);
